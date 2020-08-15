cfe185970796aba04c79300b06ab528e3f368bd9
AndyBar-NN <55596113+AndyBar-NN@users.noreply.github.com>
2020-08-15 15:37:09 +0300

    Add files via upload

---
+++ precache-manifest.f7ccf05292fcfc6085e1f5733c61ff96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "825aa76c1372dc98c699d4c65f704256",
+    "url": "/money_calc/index.html"
+  },
+  {
+    "revision": "37d471847dbb715863fc",
+    "url": "/money_calc/static/css/main.9e034465.chunk.css"
+  },
+  {
+    "revision": "6866e1971fe391653f0b",
+    "url": "/money_calc/static/js/2.7b7659b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/money_calc/static/js/2.7b7659b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37d471847dbb715863fc",
+    "url": "/money_calc/static/js/main.08c2e2e0.chunk.js"
+  },
+  {
+    "revision": "be03148f4c54c47be954",
+    "url": "/money_calc/static/js/runtime-main.efdcf1d8.js"
+  }
+]);
